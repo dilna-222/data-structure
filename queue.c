36b8a265ac6eb3a7937afe219c5ce49165df6ed8
dilna-222 <dilnasherin1@gmail.com>
2026-09-23 10:12:14 +0530

    completed

---
+++ queue.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 10
+int queue[SIZE];
+int front=0,rear=0;
+void main()
+{
+void enqueue(int);
+int dequeue(),item,opt;
+void display();
+do{
+printf("\n1.insert\n2.delete\n3.display\n4.exit\n");
+printf("Enter your choice:");
+scanf("%d",&opt);
+switch(opt)
+{
+case 1:
+printf("Enter your item:");
+scanf("%d",&item);
+enqueue(item);
+break;
+case 2:
+item=dequeue();
+if(item!=-9)
+printf("deleted value=%d\n",item);
+break;
+case 3:
+display();
+break;
+case 4:
+exit(0);
+}
+}
+while(9);
+}
+void enqueue(int item)
+{
+int temp;
+temp=(rear+1)%SIZE;
+if(temp==front)
+printf("Queue is full!");
+else{
+rear=temp;
+queue[rear]=item;
+}
+return;
+}
+int dequeue(){
+if(front==rear){
+printf("Queue is empty!");
+return -9;
+}
+else{
+front=(front+1)%SIZE;
+return queue[front];
+}
+}
+void display()
+{
+int i;
+if(front==rear)
+printf("Queue is empty!");
+else{
+i=(front+1)%SIZE;
+do
+{
+printf("%d ",queue[i]);
+i=i+1%SIZE;
+}
+while(i!=(rear+1)%SIZE);
+}
+return;
+}
